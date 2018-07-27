950af233390ab93a128c6a1767c3a9a52c8ad901
Rich Gwozdz <rich.gwozdz@gmail.com>
2018-07-27 07:57:19 -0700

    Fixes to pass standard.

--- test/exportStream.js
+++ test/exportStream.js
@@ -5,12 +5,12 @@ var ExportStream = require('../lib/exportStream')
 describe('When creating an export stream', function () {
   it('should emit an error when postgres complains', function (done) {
     ExportStream.create('foo', 'foo', {})
-    .on('error', function (err) {
-      should.exist(err)
-      done()
-    })
-    .map(function (features) {
-      throw new Error('Fail')
-    })
+      .on('error', function (err) {
+        should.exist(err)
+        done()
+      })
+      .map(function (features) {
+        throw new Error('Fail')
+      })
   })
 })
